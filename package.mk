816db8960ed5087602945ee768393e63871f9ddf
Timo Savola <timo.savola@somia.fi>
2012-03-08 17:04:29 +0200

    fix should-package-be-rebuilt check

--- package.mk
+++ package.mk
@@ -1,9 +1,9 @@
 PACKAGE_NAME	:= $(SOURCE_NAME)-$(PACKAGE_DIR)
 PACKAGE_COMMIT	:= $(shell git log -1 --pretty=format:%H -- $(PACKAGE_DIR))
 PACKAGE_VERSION	:= $(shell git describe --tags --long $(PACKAGE_COMMIT) | sed s/-/./)
-PACKAGE_FILE	:= build/$(PACKAGE_NAME)_$(PACKAGE_VERSION)_all.deb
+PACKAGE_FILE	:= packages/$(PACKAGE_NAME)_$(PACKAGE_VERSION)_all.deb
 
-$(PACKAGE_FILE): $(find $(PACKAGE_DIR) -not -type d)
+$(PACKAGE_FILE): $(shell find $(PACKAGE_DIR) -not -type d)
 	rm -rf build/$(PACKAGE_DIR)
 	mkdir -p build/$(PACKAGE_DIR)/debian packages
 	cp -r debian/. build/$(PACKAGE_DIR)/debian/
